441914c7c584b9861b2a1e53b7bd48e9d5468517
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-03-09 12:47:54 +0100

    client http

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
-import socket
+import requests
 import time
-from src.protocol import cts as proto
+from src.protocol.proto import Status
 from src.client.alphabot.untimed import AlphaBotUntimed
 from sys import argv
 
 directions_map = {
-    'B': lambda utb, am: utb.backward(am),
-    'F': lambda utb, am: utb.forward(am),
-    'R': lambda utb, am: utb.rotate(am),
-    'L': lambda utb, am: utb.rotate(-1 * am),
+    "B": lambda utb, am: utb.backward(am),
+    "F": lambda utb, am: utb.forward(am),
+    "R": lambda utb, am: utb.rotate(am),
+    "L": lambda utb, am: utb.rotate(-1 * am),
 }
 
 
@@ -16,20 +16,29 @@ def handle_path(path):
     utb = AlphaBotUntimed(_d_scale=3)
     for _, direction, amount in path:
         directions_map[direction](utb, int(amount))
-    print('_d_ waiting for key')
+    print("_d_ waiting for key")
     input()
 
 
 def handle_error(status, msg):
-    print(f'Error {status} :: {msg}')
+    print(f"Error {status} :: {msg}")
 
 
 if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
-        connection.connect(('127.0.0.1', 3000))
-        connection.send(proto.build(argv[1], argv[2]).encode())
-        status, data = proto.parse(connection.recv(1024).decode())
-        if not status == proto.Status.OK:
-            handle_error(status, data)
-        else:
-            handle_path(data)
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
+    #     connection.connect(("127.0.0.1", 3000))
+    #     connection.send(proto.build(argv[1], argv[2]).encode())
+    #     status, data = proto.parse(connection.recv(1024).decode())
+    #     if not status == proto.Status.OK:
+    #         handle_error(status, data)
+    #     else:
+    #         handle_path(data)
+    res = requests.get(f"http://127.0.0.1:3000/{argv[1]}/{argv[2]}").json()
+
+    status = Status(res["code"])
+    data = res["data"]
+
+    if not status == Status.OK:
+        handle_error(status, data)
+    else:
+        handle_path(data)
